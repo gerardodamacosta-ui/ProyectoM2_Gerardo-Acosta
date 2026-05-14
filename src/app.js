2f7f74fd07975ee2d85701d3087a5d46cd9b5087
Gerardo Acosta <gerardo.dam.acosta@gmail.com>
2026-05-14 17:38:13 -0300

    feat: conecto rutas de authors y posts en la app principal

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
+const authorsRouter = require("./routes/authors.routes");
+const postsRouter = require("./routes/posts.routes");
+const errorHandler = require("./middlewares/errorHandler");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "¡Bienvenidos a MiniBlog GerAPI!" });
+  res.json({ message: "¡Bienvenido a MiniBlog API!" });
 });
+
+app.use("/authors", authorsRouter);
+app.use("/posts", postsRouter);
+
+app.use(errorHandler);
 
 module.exports = app;
