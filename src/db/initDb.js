8a9932d8e7ab672c529a8788a06e7668122f9816
Gerardo Acosta <gerardo.dam.acosta@gmail.com>
2026-05-18 18:05:15 -0300

    fix: añado la carga del script de semillas en la inicialización de la base de datos

--- src/db/initDb.js
+++ src/db/initDb.js
@@ -3,10 +3,14 @@ const path = require("path");
 const pool = require("./pool");
 
 const setupSqlPath = path.join(__dirname, "../../sql/setup.sql");
+const seedSqlPath = path.join(__dirname, "../../sql/seed.sql");
 
 const initializeDatabase = async () => {
   const setupSql = fs.readFileSync(setupSqlPath, "utf8");
   await pool.query(setupSql);
+
+  const seedSql = fs.readFileSync(seedSqlPath, "utf8");
+  await pool.query(seedSql);
 };
 
 module.exports = { initializeDatabase };
