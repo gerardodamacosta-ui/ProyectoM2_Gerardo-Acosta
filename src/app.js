c965b24b589a4c7a9e5625417ee775d2f6dbc2eb
Gerardo Acosta <gerardo.dam.acosta@gmail.com>
2026-05-18 02:31:51 -0300

    feat: agrego soporte para Swagger UI y carga de especificación OpenAPI

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const yaml = require("yaml");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
+
+// Cargar especificación OpenAPI
+const openApiPath = path.join(__dirname, "../openapi.yaml");
+const openApiYaml = fs.readFileSync(openApiPath, "utf8");
+const swaggerSpec = yaml.parse(openApiYaml);
 
 const authorsRouter = require("./routes/authors.routes");
 const postsRouter = require("./routes/posts.routes");
@@ -10,6 +19,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "¡Bienvenido a MiniBlog API!" });
 });
+
+// Swagger UI - Documentación interactiva
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/authors", authorsRouter);
 app.use("/posts", postsRouter);
